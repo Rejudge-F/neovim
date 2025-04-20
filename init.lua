04ca1cd5d050455a7f50fdfdd83bd02342c3a988
zhangfeng.z <zhangfeng.z@bytedance.com>
2025-04-21 00:54:59 +0800

    update

--- init.lua
+++ init.lua
@@ -74,39 +74,96 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     spec = {
         {
-            "kyazdani42/nvim-tree.lua",
-            dependencies = "kyazdani42/nvim-web-devicons",
-            config = function()
-                require("nvim-tree").setup {
-                    update_focused_file = {
-                        enable = true,
-                        update_cwd = true,
-                    },
-                    filters = {
-                        dotfiles = true,
-                        custom = { "__pycache__" },
-                        exclude = { ".codebase", ".venv" }
-                    },
-                    view = {
-                        width = 30,
-                        side = "left",
-                    },
-                    git = {
-                        enable = true,
-                        ignore = false,
-                    },
-                    on_attach = function(bufnr)
-                        local api = require("nvim-tree.api")
-                        local opts = { noremap = true, silent = true, nowait = true, buffer = bufnr }
-                        api.config.mappings.default_on_attach(bufnr)
-                        vim.keymap.set('n', 'sv', api.node.open.vertical, opts)
-                        vim.keymap.set('n', 'sp', api.node.open.horizontal, opts)
+            "nvim-neo-tree/neo-tree.nvim",
+            branch = "v3.x",
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+                "nvim-tree/nvim-web-devicons", -- 注意：这里仍然使用 nvim-web-devicons
+                "MunifTanjim/nui.nvim",
+                {
+                    "s1n7ax/nvim-window-picker", -- for open_with_window_picker keymaps
+                    version = "2.*",
+                    config = function()
+                        require("window-picker").setup({
+                            filter_rules = {
+                                include_current_win = false,
+                                autoselect_one = true,
+                                -- filter using buffer options
+                                bo = {
+                                    -- if the file type is one of following, the window will be ignored
+                                    filetype = { "neo-tree", "neo-tree-popup", "notify" },
+                                    -- if the buffer type is one of following, the window will be ignored
+                                    buftype = { "terminal", "quickfix" },
+                                },
+                            },
+                        })
                     end,
-                    hijack_cursor = true,
-                    respect_buf_cwd = true,
-                    sync_root_with_cwd = true,
-                }
-                vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+                },
+            },
+            config = function()
+                require("neo-tree").setup({
+                    close_if_last_window = true,
+                    popup_border_style = "rounded",
+                    enable_git_status = true,
+                    enable_diagnostics = true,
+                    default_component_configs = {
+                        indent = {
+                            indent_size = 2,
+                            padding = 1,
+                        },
+                        git_status = {
+                            symbols = {
+                                -- 更改 git 状态符号
+                                added     = "✚",
+                                deleted   = "✖",
+                                modified  = "",
+                                renamed   = "➜",
+                                untracked = "?",
+                                ignored   = "◌",
+                                unstaged  = "",
+                                staged    = "✓",
+                                conflict  = "",
+                            }
+                        },
+                    },
+                    modified = {
+                        symbol = "[+]",
+                        highlight = "NeoTreeModified",
+                    },
+                    window = {
+                        width = 30,
+                        mappings = {
+                            ["sv"] = "open_vsplit",
+                            ["sp"] = "open_split",
+                            ["o"] = "open_with_window_picker",
+                            ["<cr>"] = "open",
+                        }
+                    },
+                    filesystem = {
+                        filtered_items = {
+                            visible = false, -- 默认不显示隐藏文件
+                            hide_dotfiles = true,
+                            hide_gitignored = true,
+                            hide_by_name = {
+                                "__pycache__",
+                                ".DS_Store",
+                            },
+                            always_show_by_pattern = {
+                                ".gitignore",
+                                "*codebase*",
+                                ".venv",
+                            },
+                        },
+                        follow_current_file = {
+                            enabled = true, -- 自动跟随当前文件
+                        },
+                        hijack_netrw_behavior = "open_current",
+                        use_libuv_file_watcher = true,
+                    },
+                })
+
+                vim.g.neo_tree_window_picker_delay = 100 -- 毫秒
+                vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>', { noremap = true, silent = true })
             end
         },
         {
