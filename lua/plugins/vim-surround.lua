14cd95e78b5951f396db912527b8f9afcdec1145
zhangfeng.z <zhangfeng.z@bytedance.com>
2025-05-03 21:59:13 +0800

    update

--- lua/plugins/vim-surround.lua
+++ lua/plugins/vim-surround.lua
@@ -1,6 +1,7 @@
 return {
     "tpope/vim-surround",
-    keys = { { "cs", mode = "n" },
+    keys = {
+        { "cs", mode = "n" },
         { "ds", mode = "n" },
         { "ys", mode = { "n", "x" } },
         { "S",  mode = "x" },
