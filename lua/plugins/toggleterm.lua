ecc0164039b0ee4453769c289c0a4e907016159f
zhangfeng.z <zhangfeng.z@bytedance.com>
2025-05-19 23:45:44 +0800

    update

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,7 +4,7 @@ return {
         require("toggleterm").setup {
             shade_terminals = true,
             shading_factor = '1',
-            start_in_insert = true,
+            start_in_insert = false,
             persist_size = true,
             direction = 'horizontal',
         }
@@ -19,6 +19,18 @@ return {
         vim.api.nvim_set_keymap('n', '<C-3>', ':3ToggleTerm<CR>',
             { noremap = true, silent = true })
         vim.api.nvim_set_keymap('n', '<C-4>', ':4ToggleTerm<CR>',
+            { noremap = true, silent = true })
+        function _G.toggle_term_height()
+            local winid = vim.api.nvim_get_current_win()
+            local cur_height = vim.api.nvim_win_get_height(winid)
+            local total_lines = vim.o.lines
+            local target_height = cur_height < math.floor(total_lines * 0.8) and math.floor(total_lines * 0.8) or 15
+            vim.api.nvim_win_set_height(winid, target_height)
+        end
+
+        vim.api.nvim_set_keymap('t', '<C-0>', [[<C-\><C-n>:lua toggle_term_height()<CR>i]],
+            { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<C-0>', [[<C-\><C-n>:lua toggle_term_height()<CR>i]],
             { noremap = true, silent = true })
     end
 }
