88bb8a2c26c20d27115d67b93e11febd874269c9
zhangfeng.z <zhangfeng.z@bytedance.com>
2025-06-18 11:03:57 +0800

    update

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -10,7 +10,7 @@ return {
         vim.keymap.set('n', 'K', ':Lspsaga peek_definition<CR>', { noremap = true, silent = true })
         vim.keymap.set('n', 'so', ':Lspsaga outline<CR>', { noremap = true, silent = true })
         vim.keymap.set('n', '<leader>ca', ':Lspsaga code_action<CR>', { noremap = true, silent = true })
-        vim.keymap.set('n', 'gr', ':Lspsaga incoming_calls<CR>', { noremap = true, silent = true })
+        vim.keymap.set('n', 'gr', ':Lspsaga finder<CR>', { noremap = true, silent = true })
         vim.keymap.set('n', 'go', ':Lspsaga outgoing_calls<CR>', { noremap = true, silent = true })
     end,
     dependencies = {
