ef8d15218054f0dbcf45c2aec02b49ead7a385bf
zhangfeng.z <zhangfeng.z@bytedance.com>
2025-06-20 09:52:13 +0800

    update

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,10 +1,23 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",  -- required
+        "sindrets/diffview.nvim", -- optional - Diff integration
 
-    -- Only one of these is needed.
-    "nvim-telescope/telescope.nvim",
-  },
+        -- Only one of these is needed.
+        "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+        require("neogit").setup({
+            kind = "floating",     -- 设置 popup 默认类型为 floating
+            -- Floating window style
+            floating = {
+                relative = "editor",
+                width = 0.8,
+                height = 0.7,
+                style = "minimal",
+                border = "rounded",
+            },
+        })
+    end
 }
