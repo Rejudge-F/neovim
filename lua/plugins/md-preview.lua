4b1cf7ad5e1fccc744d9bd82c9e0c93a768a69a3
Developer <dev@example.com>
2026-04-07 21:39:18 +0800

    chore(plugins): remove md-preview
    Remove md-preview plugin configuration.
    
    Co-authored-by: lbs-delta <lbs-delta@bytedance.com>

--- lua/plugins/md-preview.lua
+++
@@ -1,8 +0,0 @@
-return {
-  dir = "~/projects/md-preview.nvim",
-  ft = "markdown",
-  cmd = { "MarkdownPreview", "MarkdownPreviewToggle", "MarkdownPreviewBuild" },
-  build = "cd server && npm install",
-  keys = { { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown preview" } },
-  opts = {},
-}
